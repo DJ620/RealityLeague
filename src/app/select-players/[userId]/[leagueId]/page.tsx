e6abd3fe2255b1879e3f0a178f9d5b190bdb609a
DJ Hersh <DanielJHersh@gmail.com>
2024-05-20 13:30:02 -0700

    created dropdown to select players from

--- src/app/select-players/[userId]/[leagueId]/page.tsx
+++ src/app/select-players/[userId]/[leagueId]/page.tsx
@@ -1,10 +1,33 @@
+import { getLeagueInfo } from "@/app/api/leagues/actions";
+import { IPlayer } from "@/app/models/Player";
+import Loader from "@/components/Loader";
 import { ObjectId } from "mongoose";
 import React from "react";
 
-export default function SelectPlayers({
+export default async function SelectPlayers({
   params,
 }: {
   params: { userId: string; leagueId: ObjectId };
 }) {
-  return <div>Select Players for {params.userId}</div>;
+  let loading = true;
+  const leagueInfo = await getLeagueInfo(params.leagueId);
+  console.log(leagueInfo.players);
+  loading = false;
+  return (
+    <>
+      <Loader loading={loading} />
+      <div>
+        <h1 className="text-4xl">Select players for {leagueInfo.name}</h1>
+        <select name="players" id="player-select" className="text-black p-1 w-52">
+          {leagueInfo.players.map((player: IPlayer) => {
+            return (
+              <option key={player._id} value={player.name}>
+                {player.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </>
+  );
 }
